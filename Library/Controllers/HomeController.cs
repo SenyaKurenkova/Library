809fd0633daa759d5d6fc994e701b870b37caae5
Senya <senyakurenkova@gmail.com>
2022-01-08 00:10:26 +0500

    lab 1

---
+++ Library/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Library.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        //GET /HomeContoller/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //
+        //GET /HomeContoller/Welcome/
+        public string Welcome(string name, int id = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+        }
+    }
+}
